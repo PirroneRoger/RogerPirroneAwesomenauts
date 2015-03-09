e64e17d9afeac7aa4c8d74166e9ba6080bd7e71d
Roger Pirrone <r.j.pirrone.4@gmail.com>
2015-03-09 12:14:29 -0700

    did a few videos they were long

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -16,6 +16,7 @@ game.PlayerEntity = me.Entity.extend({
         
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+        this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
     
         this.renderable.setCurrentAnimation("idle");
     },
@@ -27,16 +28,44 @@ game.PlayerEntity = me.Entity.extend({
             //me.timer.tick makes the movement look smooth
             this.body.vel.x += this.body.accel.x * me.timer.tick;
             this.flipX(true);
+        }else if(me.input.isKeyPressed("left")){
+            this.body.vel.x -=this.body.accel.x * me.timer.tick;
+            this.flip.X(false);
         }else{
             this.body.vel.x = 0;
         }
         
-        if(this.body.vel.x !=0){
+        if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling){
+            this.jumping = true;
+            this.body.vel.y -= this.body.accel.y * me.timer.tick;
+        }
+        
+        
+        if (me.input.isKeyPressed("attack")) {
+            if (!this.renderable.isCurrentAnimation("attack")) {
+                //sets the current animation to attack and once thats over goes back to idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //makes it so  that the next time we start this sequence we begin from the first animation,
+                //not wherever we left off when we switched to another 
+                this.renderable.setAnimationFrame();
+            }
+        }
+        else if(this.body.vel.x !=0){
             if (!this.renderable.isCurrentAnimation("walk")) {
                 this.renderable.setCurrentAnimation("walk");
             }
         }else{
             this.renderable.setCurrentAnimation("idle");
+        }
+        
+                if(me.input.isKeyPressed("attack")){
+            if(!this.renderable.isCurrentAnimation("attack")){
+                //sets the current animation to attack and once thats over goes back to idle animation
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //makes it so  that the next time we start this sequence we begin from the first animation,
+                //not wherever we left off when we switched to another 
+                this.renderable.setAnimationFrame();
+            }
         }
         
         
@@ -56,7 +85,7 @@ game.PlayerBaseEntity = me.Entity.extend({
                 spritewidth: "100",
                 spriteheight: "100",
                 getShape: function(){
-                    return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                    return (new me.Rect(0, 0, 100, 70)).toPolygon();
                 }
         }]);
         this.broken = false;
@@ -98,7 +127,7 @@ game.EnemyBaseEntity = me.Entity.extend({
                 spritewidth: "100",
                 spriteheight: "100",
                 getShape: function(){
-                    return (new me.Rect(0, 0, 100, 100)).toPolygon();
+                    return (new me.Rect(0, 0, 100, 70)).toPolygon();
                 }
         }]);
         this.broken = false;
