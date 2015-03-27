80c25bdfcb965637b91b47a7a1c9a2887d4c3570
Roger Pirrone <r.j.pirrone.4@gmail.com>
2015-03-27 10:20:31 -0700

    finished milestone 11

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -17,6 +17,7 @@ game.GameTimerManager = Object.extend({
     goldTimerCheck: function() {
         if (Math.round(this.now / 1000) % 20 === 0 && (this.now - this.lastCreep >= 1000)) {
             game.data.gold += 1;
+            console.log("Current Gold: " + game.data.gold);
         }
     },
     
@@ -46,13 +47,13 @@ game.HeroDeathManager =  Object.extend({
 game.ExperienceManager = Object.extend({
     init: function(x, y, settings){
         this.alwaysUpdate = true;
-        this.gameOver = false;
+        this.gameover = false;
     },
     
     update: function(){
-        if(game.data.win === true && !this.gameOver){
+        if(game.data.win === true && !this.gameover){
             this.gameOver(true);
-        }else if(game.data.win === false && !this.gameOver){
+        }else if(game.data.win === false && !this.gameover){
             this.gameOver(false);
         }
         console.log(game.data.exp);
@@ -66,7 +67,7 @@ game.ExperienceManager = Object.extend({
         }else{
             game.data.exp += 1;
         }
-        this.gameOver = true;
+        this.gameover = true;
         me.save.exp = game.data.exp;
     }
 });
