d321465ebfe3af34922110d54371f406adf0e944
Roger Pirrone <r.j.pirrone.4@gmail.com>
2015-03-30 12:08:26 -0700

    finished video 42

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -1,20 +1,19 @@
-game.TitleScreen = me.ScreenObject.extend({
+game.SpendExp = me.ScreenObject.extend({
 	/**	
 	 *  action to perform on state change
 	 */
 	onResetEvent: function() {	
-		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
                 
                 
                 me.game.world.addChild(new (me.Renderable.extend({
                     init: function(){
                         this._super(me.Renderable, 'init', [270, 240, 300, 50]);
                         this.font = new me.Font("Helvetica", 46,  "white");
-                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true)
                     },
                     
                     draw: function(renderer){
-                        this.font.draw(renderer.getContext(), "Start a New Game", this.pos.x, this.pos.y);
+                        this.font.draw(renderer.getContext(), "Spend", this.pos.x, this.pos.y);
                     },
                     
                     update: function(dt){
@@ -36,25 +35,10 @@ game.TitleScreen = me.ScreenObject.extend({
                     init: function(){
                         this._super(me.Renderable, 'init', [380, 340, 250, 50]);
                         this.font = new me.Font("Helvetica", 46,  "white");
-                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true)
                     },
                     
                     draw: function(renderer){
                         this.font.draw(renderer.getContext(), "Continue", this.pos.x, this.pos.y);
-                    },
-                    
-                    update: function(dt){
-                        return true;
-                    },
-                    
-                    newGame: function(){
-                        game.data.exp = me.save.exp;
-                        game.data.exp1 = me.save.exp1;
-                        game.data.exp2 = me.save.exp2;
-                        game.data.exp3 = me.save.exp3;
-                        game.data.exp4 = me.save.exp4; 
-                        me.input.releasePointerEvent('pointerdown', this);
-                        me.state.change(me.state.SPENDEXP);
                     }
                 })));
                 
