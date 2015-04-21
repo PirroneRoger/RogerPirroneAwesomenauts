ec4b8d0144f3c898281ef0613684b94e595ccd2d
Roger Pirrone <r.j.pirrone.4@gmail.com>
2015-04-21 11:24:42 -0700

    finished intermediate video 2

--- js/screens/title.js
+++ js/screens/title.js
@@ -1,61 +1,62 @@
 game.TitleScreen = me.ScreenObject.extend({
-	/**	
-	 *  action to perform on state change
-	 */
-	onResetEvent: function() {	
-		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
-                
-                
-                me.game.world.addChild(new (me.Renderable.extend({
-                    init: function(){
-                        this._super(me.Renderable, 'init', [270, 240, 300, 50]);
-                        this.font = new me.Font("Helvetica", 46,  "white");
-                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true)
-                    },
-                    
-                    draw: function(renderer){
-                        this.font.draw(renderer.getContext(), "Start a New Game", this.pos.x, this.pos.y);
-                    },
-                    
-                    update: function(dt){
-                        return true;
-                    },
-                    
-                    newGame: function(){
-                        me.input.releasePointerEvent('pointerdown', this);
-                        me.state.change(me.state.NEW);
-                    }
-                })));
-                
-                    me.game.world.addChild(new (me.Renderable.extend({
-                    init: function(){
-                        this._super(me.Renderable, 'init', [380, 340, 250, 50]);
-                        this.font = new me.Font("Helvetica", 46,  "white");
-                        me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true)
-                    },
-                    
-                    draw: function(renderer){
-                        this.font.draw(renderer.getContext(), "Continue", this.pos.x, this.pos.y);
-                    },
-                    
-                    update: function(dt){
-                        return true;
-                    },
-                    
-                    newGame: function(){
-                        me.input.releasePointerEvent('pointerdown', this);
-                        me.state.change(me.state.LOAD);
-                    }
-                })));
-                
-                
-	},
-	
-	
-	/**	
-	 *  action to perform when leaving this screen (state change)
-	 */
-	onDestroyEvent: function() {
+    /**	
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage("title-screen")), -10);
 
-	}
+        game.data.option1 = new (me.Renderable.extend({
+            init: function() {
+                this._super(me.Renderable, 'init', [270, 240, 300, 50]);
+                this.font = new me.Font("Arial", 46, "white");
+                me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+
+            },
+            draw: function(renderer) {
+                this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
+
+            },
+            update: function(dt) {
+                return true;
+            },
+            newGame: function() {
+                me.input.releasePointerEvent('pointerdown', this);
+                me.input.releasePointerEvent('pointerdown', game.data.option2);
+                me.state.change(me.state.NEW);
+            }
+        }));
+        me.game.world.addChild(game.data.option1);
+
+        me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage("title-screen")), -10);
+
+       game.data.option2 = new (me.Renderable.extend({
+            init: function() {
+                this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+                this.font = new me.Font("Arial", 46, "white");
+                me.input.registerPointerEvent('pointerdown', this, this.continueGame.bind(this), true);
+
+            },
+            draw: function(renderer) {
+                this.font.draw(renderer.getContext(), "CONTINUE GAME", this.pos.x, this.pos.y);
+
+            },
+            update: function(dt) {
+                return true;
+            },
+            continueGame: function() {
+                me.input.releasePointerEvent('pointerdown', game.data.option1);
+                me.input.releasePointerEvent('pointerdown', this);
+                me.state.change(me.state.LOAD);
+            }
+        }));
+        me.game.world.addChild(game.data.option2);
+
+
+    },
+    /**	
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+        
+    }
 });
